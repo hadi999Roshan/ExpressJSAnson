1860c4d5b1d47038f402687f6ef2b6920126be9c
Hadi Roshan <109845464+hadi999Roshan@users.noreply.github.com>
2025-02-04 10:29:43 +0330

    Route Parameters

--- src/index.mjs
+++ src/index.mjs
@@ -8,16 +8,30 @@ const app = express();
 //process is global in node.js and has an object caleed env (environment) , if the PORT object is not assigned, we will assign 3000
 const PORT = process.env.PORT || 3000;
 
+const mockUsers = [
+  { id: 0, username: "teo", displayName: "Teo" },
+  { id: 1, username: "cane", displayName: "Cane" },
+  { id: 2, username: "amenadiel", displayName: "Amenadiel" },
+];
+
 app.get("/", (request, response) => {
   response.status(201).send("Hello World!");
 });
 
 app.get("/api/users", (request, response) => {
-  response.send([
-    { id: 0, username: "teo", displayName: "Teo" },
-    { id: 1, username: "cane", displayName: "Cane" },
-    { id: 2, username: "amenadiel", displayName: "Amenadiel" },
-  ]);
+  response.send(mockUsers);
+});
+
+//id here is a route parameter
+app.get("/api/users/:id", (request, response) => {
+  console.log(request.params);
+  const parsedID = parseInt(request.params.id);
+  console.log(parsedID);
+  if (isNaN(parsedID))
+    return response.status(400).send({ msg: "Bad Request Inavlid ID!" });
+  const findUser = mockUsers.find((user) => user.id === parsedID);
+  if (!findUser) return response.sendStatus(404);
+  return response.send(findUser);
 });
 
 app.get("/api/products", (request, response) => {
