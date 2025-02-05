248cd9bce5d5d57c9b952a641683d9bb079ffc81
Hadi Roshan <109845464+hadi999Roshan@users.noreply.github.com>
2025-02-05 09:46:33 +0330

    PATCH Request

--- src/index.mjs
+++ src/index.mjs
@@ -94,6 +94,20 @@ app.put("/api/users/:id", (request, response) => {
   return response.sendStatus(200);
 });
 
+app.patch("/api/users/:id", (request, response) => {
+  const {
+    body,
+    params: { id },
+  } = request;
+  const parsedId = parseInt(id);
+  if (isNaN(parsedId)) return response.sendStatus(400); // Fix typo here
+
+  const findUserIndex = mockUsers.findIndex((user) => user.id === parsedId);
+  if (findUserIndex === -1) return response.sendStatus(404);
+  mockUsers[findUserIndex] = { ...mockUsers[findUserIndex], ...body };
+  return response.sendStatus(200);
+});
+
 app.listen(PORT, () => {
   console.log(`Running on port ${PORT}`);
 });
